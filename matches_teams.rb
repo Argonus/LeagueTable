6b5bfb0fa9b9bd9b3f914a84bfea3fb39d51c456
Argonus <argonuspiotr@gmail.com>
2016-03-17 19:24:33 +0100

    clean

--- matches_teams.rb
+++ matches_teams.rb
@@ -7,26 +7,31 @@ class MatchesTeam
 
   def push(match_info)
     info = match_info.split(/\s-\s/)
-    extract_team_result_info(info)
+    set_match_result(info)
   end
 
   def add_team(team_name)
-    update_or_create_team(team_name)
+    create_team(team_name, 0, 0, 0, 0, 0, 0)
   end
 
   private
 
-  def extract_team_result_info(info)
+  def set_match_result(info)
+    two_teams = extract_teams_goals_from_match(info)
+    set_teams_score(two_teams)
+  end
+
+  def extract_teams_goals_from_match(info)
     two_teams = []
     info.each do |team_info|
       team_name = team_info.scan(/[a-zA-Z\s-]/).join('').strip
       goals = team_info.scan(/\d+/).join('').to_i
       two_teams << [team_name, goals]
     end
-    set_match_score(two_teams)
+    two_teams
   end
 
-  def set_match_score(two_teams)
+  def set_teams_score(two_teams)
     if two_teams[0][1] > two_teams[1][1]
       update_or_create_team(two_teams[0][0], 3, two_teams[0][1], 1, 0, 0, -two_teams[1][1])
       update_or_create_team(two_teams[1][0], 0, two_teams[1][1], 0, 0, 1, -two_teams[0][1])
@@ -54,10 +59,8 @@ class MatchesTeam
   end
 
   def update_team(team_name, *args)
-    i = 0
-    @team_list[team_name].each do |k, v|
+    @team_list[team_name].each_key.with_index do |k, i|
       @team_list[team_name][k] += args[i]
-      i += 1
     end
   end
 
